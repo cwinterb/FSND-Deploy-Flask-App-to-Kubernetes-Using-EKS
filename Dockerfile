31ce2111acf32f1a76674a4fda68740024968eae
Carmella Winterbauer <carmella@onehopewine.com>
2019-12-27 14:20:26 -0800

    entrypoint

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ COPY . /app
 WORKDIR /app
 RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
-CMD ["gunicorn", "-b", ":8080", "main:APP"]
+ENTRYPOINT ["gunicorn", "-b", ":8080", "main:APP"]
 
-# todo again 
+# todo again  
--- main.py
+++ main.py
@@ -18,10 +18,10 @@ LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
 def _logger():
     '''
     Setup logger format, level, and handler.
-
     RETURNS: log object
     '''
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
     log = logging.getLogger(__name__)
     log.setLevel(LOG_LEVEL)
@@ -34,8 +34,9 @@ def _logger():
 
 
 LOG = _logger()
-LOG.debug("Starting with log level: %s" % LOG_LEVEL )
+LOG.debug("Starting with log level: %s" % LOG_LEVEL)
 APP = Flask(__name__)
+
 
 def require_jwt(function):
     """
@@ -43,13 +44,14 @@ def require_jwt(function):
     """
     @functools.wraps(function)
     def decorated_function(*args, **kws):
+        print("require_jwt")
         if not 'Authorization' in request.headers:
             abort(401)
         data = request.headers['Authorization']
         token = str.replace(str(data), 'Bearer ', '')
         try:
             jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
-        except: # pylint: disable=bare-except
+        except:  # pylint: disable=bare-except
             abort(401)
 
         return function(*args, **kws)
@@ -68,17 +70,21 @@ def auth():
     """
     request_data = request.get_json()
     email = request_data.get('email')
+    print(email)
     password = request_data.get('password')
+    print(password)
     if not email:
+        print("not email")
         LOG.error("No email provided")
         return jsonify({"message": "Missing parameter: email"}, 400)
     if not password:
+        print("not password")
         LOG.error("No password provided")
         return jsonify({"message": "Missing parameter: password"}, 400)
     body = {'email': email, 'password': password}
-
+    print(body)
     user_data = body
-
+    print(user_data)
     return jsonify(token=_get_jwt(user_data).decode('utf-8'))
 
 
@@ -87,19 +93,19 @@ def decode_jwt():
     """
     Check user token and return non-secret data
     """
+    print("contents")
     if not 'Authorization' in request.headers:
         abort(401)
     data = request.headers['Authorization']
     token = str.replace(str(data), 'Bearer ', '')
     try:
         data = jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
-    except: # pylint: disable=bare-except
+    except:  # pylint: disable=bare-except
         abort(401)
-
 
     response = {'email': data['email'],
                 'exp': data['exp'],
-                'nbf': data['nbf'] }
+                'nbf': data['nbf']}
     return jsonify(**response)
 
 
@@ -110,5 +116,6 @@ def _get_jwt(user_data):
                'email': user_data['email']}
     return jwt.encode(payload, JWT_SECRET, algorithm='HS256')
 
+
 if __name__ == '__main__':
-    APP.run(host='127.0.0.1', port=8080, debug=True)
+    APP.run(host='127.0.0.1', port=8080, debug=False)
